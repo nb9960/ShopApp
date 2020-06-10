2d932fa4b4306db092fc119ba7d6b39ea2f4d95e
nb9960 <nbodani8@gmail.com>
2020-06-10 21:10:59 +0530

    First Commit

---
+++ lib/screens/products_overview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../widgets/products_grid.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Shop')),
+      body:ProductsGrid(),
+    );
+  }
+}
+
